d4f312bdd9d53f7cacfcf8c6ba053e76f2ac6e3d
Egor Melnichenko <egor.melnichenko@bk.ru>
2020-01-28 16:32:04 +0300

    Updates

---
+++ precache-manifest.58c7ed49624cab08d282c2fbdd6fc181.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e6aed3395022cf211b81d7beda19165",
+    "url": "/react-2019-11-14/index.html"
+  },
+  {
+    "revision": "7e4a5c0fb3b489874ab4",
+    "url": "/react-2019-11-14/static/css/2.f4d50da8.chunk.css"
+  },
+  {
+    "revision": "4937c906f5a96c831dff",
+    "url": "/react-2019-11-14/static/css/main.ab1e4553.chunk.css"
+  },
+  {
+    "revision": "7e4a5c0fb3b489874ab4",
+    "url": "/react-2019-11-14/static/js/2.95fd0b1d.chunk.js"
+  },
+  {
+    "revision": "4937c906f5a96c831dff",
+    "url": "/react-2019-11-14/static/js/main.bf7114cf.chunk.js"
+  },
+  {
+    "revision": "39952cd6454a9abedaf6",
+    "url": "/react-2019-11-14/static/js/runtime-main.61ec3c2e.js"
+  },
+  {
+    "revision": "f18dca139aa4abd2d10759050c412715",
+    "url": "/react-2019-11-14/static/media/hero-banner.f18dca13.jpg"
+  },
+  {
+    "revision": "cffb30937d763b4e21b5d03963e367fc",
+    "url": "/react-2019-11-14/static/media/logo.cffb3093.svg"
+  }
+]);
